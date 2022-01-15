2bab5c51b6d41c312ba70f899202619e297f1b81
Albert Goody <albert.goody@gmail.com>
2022-01-15 11:40:08 +0000

    completed index

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,23 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # # read all the restaurants
+  # get '/tasks', to: 'tasks#index'
+
+  # # read one restaurant
+  # get '/tasks/:id', to: 'tasks#show'
+
+  # # create a restaurant
+  # get '/tasks/new', to: 'tasks#new'
+  # post '/tasks', to: 'tasks#create'
+
+  # # update a restaurant
+  # get '/tasks/:id/edit', to: 'tasks#edit'
+  # patch '/tasks/:id', to: 'tasks#update'
+
+  # # delete a restaurant
+  # delete '/tasks:id', to: 'tasks#destroy'
+
+  resources :tasks
+
 end
