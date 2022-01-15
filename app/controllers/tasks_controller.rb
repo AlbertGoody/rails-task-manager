5221367cde2d88a40f81df37088451eac80569ba
Albert Goody <albert.goody@gmail.com>
2022-01-15 16:13:41 +0000

    completed all steps including optional

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,16 +1,14 @@
 class TasksController < ApplicationController
+  before_action :find_task, only: [:show, :edit, :update, :destroy]
 
   def index
     @tasks = Task.all
   end
 
-  def show
-    @task = Task.find(params[:id])
-  end
+  def show; end
 
   def new
     @task = Task.new
-
   end
 
   def create
@@ -19,21 +17,15 @@ class TasksController < ApplicationController
     redirect_to task_path(@task)
   end
 
-  def edit
-    @task = Task.find(params[:id])
-  end
+  def edit; end
 
   def update
-    @task = Task.find(params[:id])
     @task.update(task_params)
-
-    redirect_to task_path(@task)
+    redirect_to tasks_path(@task)
   end
 
   def destroy
-    @task = Task.find(params[:id])
     @task.destroy
-
     redirect_to tasks_path
   end
 
@@ -41,6 +33,10 @@ class TasksController < ApplicationController
 
   def task_params
     params.require(:task).permit(:title, :details, :completed)
+  end
+
+  def find_task
+    @task = Task.find(params[:id])
   end
 
 end
